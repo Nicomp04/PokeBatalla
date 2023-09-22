c008f2b45d5235333a01c4e5724f8cf07bde07e6
Nicomp04 <nicolasmp99@hotmail.com>
2023-09-22 14:37:12 -0300

    Juego

--- src/main/java/org/example/Juego.java
+++ src/main/java/org/example/Juego.java
@@ -5,20 +5,19 @@ public class Juego {
     private Jugador jugador2;
     private int turnoDe;
 
+    public Juego(Jugador jugador1, Jugador jugador2) {
+        this.jugador1 = jugador1;
+        this.jugador2 = jugador2;
+        this.turnoDe = definirPrimerTurno();
+    }
     public int definirPrimerTurno(){ // Lo pense asi, puede cambiarse mas adelante
-        pokemon1 = this.jugador1.pokemonActual();// Agregar a Jugador.java el atributo pokemonActual
-        pokemon2 = this.jugador2.pokemonActual();
+        Pokemon pokemon1 = this.jugador1.pokemonActual();// Agregar a Jugador.java el atributo pokemonActual
+        Pokemon pokemon2 = this.jugador2.pokemonActual();
         return (pokemon1.getVelocidad() < pokemon2.getVelocidad());
     }
 
     public void ejecutarTurno(){ // A implementar...
 
-    }
-
-    public Juego(Jugador jugador1, Jugador jugador2) {
-        this.jugador1 = jugador1;
-        this.jugador2 = jugador2;
-        this.turnoDe = definirPrimerTurno();
     }
 
     public Jugador getJugador1() {
