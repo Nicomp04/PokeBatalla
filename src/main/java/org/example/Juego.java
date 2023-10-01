e8532ffce142c0f4936c6e4ecd89ad6674cb7040
juancpuch <jrobledo@fi.uba.ar>
2023-09-30 21:04:05 -0300

    El jugador puede usar Turno

--- src/main/java/org/example/Juego.java
+++ src/main/java/org/example/Juego.java
@@ -9,6 +9,7 @@ public class Juego {
     private Jugador jugador1;
     private Jugador jugador2;
     private boolean turnoDe1;
+    private Jugador turnoActivo;
 
     public Juego() {
         Generador gen = new Generador();
@@ -19,14 +20,21 @@ public class Juego {
         this.jugador1 = jugador1;
         this.jugador2 = jugador2;
 
-        this.turnoDe1 = definirPrimerTurno();
+        //this.turnoDe1 = definirPrimerTurno();
+        this.turnoActivo = definirPrimerTurno();
     }
-    public boolean definirPrimerTurno(){ // Lo pense asi, puede cambiarse mas adelante
+
+    public Jugador definirPrimerTurno(){ // Lo pense asi, puede cambiarse mas adelante
         Pokemon pokemon1 = this.jugador1.pokemonActual();//
         Pokemon pokemon2 = this.jugador2.pokemonActual();
 
-        return (pokemon1.getVelocidad() < pokemon2.getVelocidad());
+        //return (pokemon1.getVelocidad() < pokemon2.getVelocidad());
+        if (pokemon1.getVelocidad() < pokemon2.getVelocidad()){
+            return jugador1;
+        }
+        return jugador2;
     }
+
 
     public void avanzarTurno(){ // A implementar...
 
@@ -43,4 +51,12 @@ public class Juego {
     public boolean getTurnoDe1() {
         return turnoDe1;
     }
+
+    public void habilitarTurno(){
+        if (turnoActivo.equals(jugador1)){
+            jugador1.usarTurno();
+        }
+        jugador2.usarTurno();
+    }
 }
+
--- src/main/java/org/example/Jugador.java
+++ src/main/java/org/example/Jugador.java
@@ -11,14 +11,47 @@ public class Jugador {
     private String nombre;
     private List<Pokemon> pokemones;
     private List<Item> items;
+    private Pokemon pokemonActivo;
 
     public Jugador(String nombre, List<Pokemon> pokemones, List<Item> items){
         this.nombre = nombre;
         this.pokemones = pokemones;
         this.items = items;
+        this.pokemonActivo = elegirPokemonActivo();
     }
 
     public Pokemon pokemonActual() {
         return pokemones.get(0);
     }
+    public void usarTurno(){
+        int accionElegida = 0; //vamos a tener q hacer una funcion cn un input para q elija lo que quiera hacer.
+        switch (accionElegida){
+            case 1:
+                usarHabilidad();
+                break;
+            case 2:
+                usarItem();
+                break;
+            case 3:
+                elegirPokemonActivo();
+                break;
+            case 4:
+                rendirse();
+                break;
+        }
+    }
+
+    private void usarHabilidad() { //falta implementar
+    }
+
+    private void usarItem() {  //falta implementar
+    }
+
+    private Pokemon elegirPokemonActivo() {  //falta implementar
+        return pokemones.get(0);
+    }
+
+    private void rendirse() { //falta implementar
+    }
 }
+
