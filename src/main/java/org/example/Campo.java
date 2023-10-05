ce165157841cd294c2f92cf52e259aa4be5412da
Franco Singh <sifranco@fi.uba.ar>
2023-10-05 10:20:49 -0300

    Agrega la implementacion de los estados dormido y paralizado

--- src/main/java/org/example/Campo.java
+++ src/main/java/org/example/Campo.java
@@ -1,16 +1,22 @@
 package org.example;
 
+import org.example.Estado.Estados;
+import org.example.Habilidades.Ataque;
 import org.example.Habilidades.Habilidad;
 import org.example.Pokemon.Pokemon;
-import org.example.Tipo.Tipo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Campo {
     private Pokemon pokemonAtacante;
     private Pokemon pokemonAtacado;
     private List<Pokemon> pokemonesActivos = new ArrayList<>();
+
+    final Logger logger = LoggerFactory.getLogger(Campo.class);
 
     public Campo(Pokemon pokemon1, Pokemon pokemon2){
         pokemonesActivos.add(pokemon1);
@@ -51,13 +57,28 @@ public class Campo {
 
     }
 
+    public boolean validarEstadoParalizado(Pokemon pokemon){
+        Random random = new Random();
+        double valorAleatorio = random.nextDouble();
+        return ((pokemon.getEstado() == Estados.PARALIZADO) && (valorAleatorio < 0.005));
+    }
+
     /*
     Identifica que habilidad se va a usar
      */
     public void elejirHabilidad (int idAtacante){
         identificarAtacante(idAtacante);
         Habilidad habilidad = pokemonAtacante.mostrarYelegirHabilidad();
-        aplicarHabilidad(habilidad);
+        validarEstadoDespierto(habilidad);
+    }
+
+    private void validarEstadoDespierto(Habilidad habilidad){
+        if(pokemonAtacante.getEstado() == Estados.DORMIDO){
+            logger.info("el pokemon esta Dormido, No Puede Atacar");
+            pokemonAtacante.cambiarEstado(null);// falta agregar lo del contador de turnos.
+        }else {
+            aplicarHabilidad(habilidad);
+        }
     }
 
     public void empezarTurno(Jugador turnoActivo) {
@@ -68,7 +89,10 @@ public class Campo {
     Segurn si afecta al enemigo o al atacante se llama a la habilidad para que se active
      */
     public void aplicarHabilidad (Habilidad habilidadElegida){
-        if (habilidadElegida.atacaAEnemigo()){
+        if(validarEstadoParalizado(pokemonAtacante)){
+            logger.info("el pokemon esta paralizado, No Puede Moverse!!!");
+        }
+        else if (habilidadElegida.atacaAEnemigo()){
             habilidadElegida.usarEnPokemon(pokemonAtacante, pokemonAtacado);
         }
         else if (habilidadElegida.afectaAEnemigo()){
