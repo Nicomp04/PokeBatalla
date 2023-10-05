fc333ca766fd535c0ccd6fd33b359fa689e42b78
Franco Singh <sifranco@fi.uba.ar>
2023-10-05 10:28:15 -0300

    Agrega la implementacion del estado Paralizado

--- src/main/java/org/example/Habilidades/Habilidad.java
+++ src/main/java/org/example/Habilidades/Habilidad.java
@@ -1,5 +1,8 @@
 package org.example.Habilidades;
+import org.example.Estado.Estados;
 import org.example.Pokemon.Pokemon;
+
+import java.util.Random;
 
 public abstract class Habilidad{
     protected String nombre;
@@ -22,5 +25,11 @@ public abstract class Habilidad{
 
     public String getNombre() {
         return this.nombre;
+    }
+
+    public boolean estadoParalizado(Pokemon pokemon){
+        Random random = new Random();
+        double valorAleatorio = random.nextDouble();
+        return ((pokemon.getEstado() == Estados.PARALIZADO) && (valorAleatorio < 0.005));
     }
 }
